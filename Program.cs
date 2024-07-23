a473b719291ae3ebdc96b1c28a421485b1b21711
suzy1997 <suzysu418@gmail.com>
2024-07-23 18:13:31 +0100

    corss wall

--- Program.cs
+++ Program.cs
@@ -4,111 +4,162 @@ using System.Numerics;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
+
 namespace HelloWorld
 {
+
+
     class Program
     {
         //window status
         const int screenWidth = 640;
         const int screenHeight = 480;
-        private static bool GameOver = false;
-        static bool pause = false;
-        static bool intialgame = false;
         static int level = 0;
+
+        static bool intialgame = true;
+        static bool startgame = false;
+        static bool endgame = false;
+        static bool pause = false;
 
         //snake status
         private static float speed=10.0f;
         private static Vector2 direction = new Vector2(10, 0);
         private static List<Vector2> SnakeBody;
         private static Vector2 HeadPosition;
-        static bool allowMove = false;
+        static bool CantTurn = false;
 
         //food status
         private static Vector2 FoodPosition;
         private static Vector2 foodPos;
         private static bool eatfood=false;
         private static int foodAcount=0;
-        
 
+        // Timer
+        private static float MoveInterval = 0.1f; // Move every 0.1 seconds
+        private static float MoveTimer = 0.0f;
 
         static void InitGame()
         {
 
             level = 0;
             foodAcount = 0;
-            GameOver = false;
+           
             pause = false;
-            allowMove = false;
-            intialgame = true;
+            CantTurn = false;
             
             // intial window
             Raylib.InitWindow(screenWidth, screenHeight, "snake game window");
+            // !when press keyboard beacause the fps value too high, is not senstive 
             Raylib.SetTargetFPS(10);
 
-
-            if(Raylib.IsKeyDown(KeyboardKey.Enter))
-            {
-                //intial status
-                GameOver = false;
-                eatfood = false;
-            }
+                
 
 
-            if (!GameOver)
-            {
-                //intial snake position
-                //HeadPosition = new Vector2(Raylib.GetRandomValue(0, screenWidth / 10) * 10, Raylib.GetRandomValue(0, screenHeight / 10) * 10);
-                HeadPosition = new Vector2(240,140);
-                SnakeBody = new List<Vector2>()
-                {
-                new Vector2(HeadPosition.X,HeadPosition.Y),
-                new Vector2(HeadPosition.X-10,HeadPosition.Y),
-                new Vector2(HeadPosition.X-20,HeadPosition.Y)
-                };
+            //intial snake position
+            //HeadPosition = new Vector2(Raylib.GetRandomValue(0, screenWidth / 10) * 10, Raylib.GetRandomValue(0, screenHeight / 10) * 10);
+            HeadPosition = new Vector2(240, 140);
+            SnakeBody = new List<Vector2>()
+                        {
+                        new Vector2(HeadPosition.X,HeadPosition.Y),
+                        new Vector2(HeadPosition.X-10,HeadPosition.Y),
+                        new Vector2(HeadPosition.X-20,HeadPosition.Y)
+                        };
 
-                //generate food
-                FoodPosition = new Vector2(130,130);
-
-
-            }
-            
+            //generate food
+            FoodPosition = new Vector2(130, 130);
 
         }
 
-        public static void Update(string[] args)
-        {
-            if (!GameOver)
-            {
 
-                if (Raylib.IsKeyDown(KeyboardKey.Enter))
+        public static void Update()
+        {
+            ChangPage();
+
+
+            if (startgame)
+            {
+                //player input
+                if (Raylib.IsKeyDown(KeyboardKey.Right) && direction != new Vector2(-10, 0) && !CantTurn)
                 {
-                    pause = !pause;
+                    direction = new Vector2(10, 0);
+
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.Left) && direction != new Vector2(10, 0) && !CantTurn)
+                {
+                    direction = new Vector2(-10, 0);
+
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.Up) && direction != new Vector2(0, 10) && !CantTurn)
+                {
+                    direction = new Vector2(0, -10);
+
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.Down) && direction != new Vector2(0, -10) && !CantTurn)
+                {
+                    direction = new Vector2(0, 10);
+
                 }
 
-                if (!pause)
+                //move timer
+                MoveTimer += Raylib.GetFrameTime();
+                if (MoveTimer >= MoveInterval)
                 {
-                    //player input
-                    if (Raylib.IsKeyDown(KeyboardKey.Right) && direction != new Vector2(-10, 0))
-                    {
-                        direction = new Vector2(10, 0);
-                    }
-                    if (Raylib.IsKeyDown(KeyboardKey.Left) && direction != new Vector2(10, 0))
-                    {
-                        direction = new Vector2(-10 , 0);
-                    }
-                    if (Raylib.IsKeyDown(KeyboardKey.Up) && direction != new Vector2(0, 10))
-                    {
-                        direction = new Vector2(0, -10 );
-                    }
-                    if (Raylib.IsKeyDown(KeyboardKey.Down) && direction != new Vector2(0, -10))
-                    {
-                        direction = new Vector2(0, 10 );
-                    }
-
+                    MoveTimer = 0.0f;
                     //snake movement
                     HeadPosition = SnakeBody[0] + direction;
+
+                    //cross wall
+                    if (HeadPosition.X < 0 || HeadPosition.X > screenWidth || HeadPosition.Y > screenHeight || HeadPosition.Y < 0)
+                    {
+                        CantTurn = true;
+                    }
+                    else
+                    { CantTurn = false; }
+
+                    if (HeadPosition.X < 0)
+                    {
+                        HeadPosition.X = screenWidth;
+                    }
+                    else if (HeadPosition.X > screenWidth)
+                    {
+                        HeadPosition.X = 0;
+                    }
+                    else if (HeadPosition.Y > screenHeight)
+                    {
+                        HeadPosition.Y = 0;
+                    }
+                    else if (HeadPosition.Y < 0)
+                    {
+                        HeadPosition.Y = screenHeight;
+                    }
+
+                    /*switch (HeadPosition.X)
+                    {
+                        case -10:
+                            HeadPosition.X = screenWidth;
+                            break;
+                        case screenWidth:
+                            HeadPosition.X = -10;
+                            break;
+
+                    }
+
+                    switch (HeadPosition.Y)
+                    {
+                        case -10:
+                            HeadPosition.Y = screenHeight;
+                            break;
+                        case screenHeight:
+                            HeadPosition.Y = -10;
+                            break;
+
+                    }*/
+
+
                     SnakeBody.RemoveAt(SnakeBody.Count - 1);
                     SnakeBody.Insert(0, HeadPosition);
+
+
 
                     //check whether eat food
                     if (Vector2.Distance(SnakeBody[0], FoodPosition) < 10)
@@ -116,52 +167,86 @@ namespace HelloWorld
                         eatfood = true;
                         SnakeBody.Add(SnakeBody[SnakeBody.Count - 1]);
                         foodAcount++;
-                        level = foodAcount / 4;
+                        level = foodAcount;
+                        //level = foodAcount / 4;
                         Generate();
-                        Raylib.SetTargetFPS(10*(1+level/2));
+                        //Raylib.SetTargetFPS(10 * (1 + level / 2));
+                        
+
+                        MoveInterval = 0.1f / (1 + (level * 0.1f));
                     }
-                }
 
-                //Game Over
-                //snakehead touch screen
-                if (HeadPosition.X == screenWidth + 10 ||
-                    HeadPosition.X == -10 ||
-                    HeadPosition.Y == screenHeight+10 || HeadPosition.Y == -10)
-                {
-                    GameOver = true; 
-
-                }
-                //snakehead touch snakebody
-                for (int i = 1; i < SnakeBody.Count; i++)
-                {
-                    if (SnakeBody[0] == SnakeBody[i])
+                    //snakehead touch snakebody
+                    for (int i = 1; i < SnakeBody.Count; i++)
                     {
-                        GameOver = true;
+                        if (SnakeBody[0] == SnakeBody[i])
+                        {
+                            endgame = true;
+                            startgame = false;
+
+                        }
                     }
                 }
 
-            }
-            else
-            {
 
-                if (Raylib.IsKeyDown(KeyboardKey.Enter))
-                {
-                    Raylib.CloseWindow();
-                    InitGame();
-                    GameOver = false;
-                }
-            }
 
+
+
+
+
+            }
         }
 
+            static void ChangPage()
+            {
+                //intial page
+                if (intialgame)
+                {
+                    if (Raylib.IsKeyDown(KeyboardKey.Enter))
+                    {
+                        //intial status                  
+                        intialgame = false;
+                        startgame = true;
+                    }
+                    return;
+                }
 
-        public static void Main(string[] args)
+                // game page
+                if (startgame)
+                {
+
+                    if (Raylib.IsKeyDown(KeyboardKey.Space))
+                    {
+                        pause = !pause;
+                    }
+                    return;
+                }
+
+                //gameover page
+                if (endgame)
+                {
+
+                    if (Raylib.IsKeyDown(KeyboardKey.Enter))
+                    {
+                        endgame = false;
+                        startgame = true;
+                        Raylib.CloseWindow();
+                        InitGame();
+                    }
+                    return;
+                }
+
+            }
+        
+
+
+            public static void Main(string[] args)
         {
             InitGame();
 
             while (!Raylib.WindowShouldClose())
             {
-                Update(args);
+                Update();
                 
                 Draw();
             }
@@ -205,7 +290,13 @@ namespace HelloWorld
 
             Raylib.ClearBackground(Color.White);
 
-            if (!GameOver)
+
+            if (intialgame)
+            {
+                Raylib.DrawText("PRESS [ENTER] TO PLAY GAME", Raylib.GetScreenWidth() / 2 - Raylib.MeasureText("PRESS [ENTER] TO PLAY GAME", 20) / 2, Raylib.GetScreenHeight() / 2 - 50, 20, Color.Gray);
+
+            }
+            else  if (startgame)
             {
                 // draw food
                 Raylib.DrawRectangleV(FoodPosition, new Vector2(10, 10), Color.Green);
@@ -215,27 +306,29 @@ namespace HelloWorld
                 {
                     Vector2 segment = SnakeBody[i];
                     //random snake color
-                    Color segmentColor = new Color(0, Raylib.GetRandomValue(0, 255), Raylib.GetRandomValue(0, 255), 255); 
+                    Color segmentColor = new Color(0, Raylib.GetRandomValue(0, 255), Raylib.GetRandomValue(0, 255), 255);
                     Raylib.DrawRectangleV(segment, new Vector2(10, 10), segmentColor);
                 }
 
                 //draw Score counting
-                Raylib.DrawText("Score:" + foodAcount , 10, 10, 30, Color.Pink);
+                Raylib.DrawText("Score:" + foodAcount, 10, 10, 30, Color.Pink);
                 Raylib.DrawText("Lvel:" + level, 10, 40, 30, Color.Pink);
+                Raylib.DrawText("MoveInterval:" + MoveInterval, 10, 70, 30, Color.Pink);
 
                 if (pause)
                 {
-                    Raylib.DrawText("GAME PAUSED", screenWidth / 2 - Raylib.MeasureText("GAME PAUSED", 40) / 2, screenHeight / 2 - 40, 40, Color.Gray); 
+                    Raylib.DrawText("GAME PAUSED", screenWidth / 2 - Raylib.MeasureText("GAME PAUSED", 40) / 2, screenHeight / 2 - 40, 40, Color.Gray);
                 }
             }
-            else
+            else if(endgame)
             {
-                Raylib.DrawText("PRESS [ENTER] TO PLAY AGAIN", Raylib.GetScreenWidth() / 2 - Raylib.MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20) / 2, Raylib.GetScreenHeight() / 2 - 50, 20, Color.Gray);
+                Raylib.DrawText("GAMEOVER! PRESS [ENTER] TO PLAY AGAIN", Raylib.GetScreenWidth() / 2 - Raylib.MeasureText("GAMEOVER! PRESS [ENTER] TO PLAY AGAIN", 20) / 2, Raylib.GetScreenHeight() / 2 - 50, 20, Color.Gray);
             }
 
-           
 
-            Raylib.EndDrawing();
+
+
+                Raylib.EndDrawing();
         }
 
 
--- obj/Debug/net8.0/Snake_Raylib.AssemblyInfo.cs
+++ obj/Debug/net8.0/Snake_Raylib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake_Raylib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4cc598b0c20e7d0eeba6efaae83003813482413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042f460a4fa9a30385376fdfa54678d261e5a6ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake_Raylib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake_Raylib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
